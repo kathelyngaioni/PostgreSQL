6713dee603cd69cccb9f299136591c574a4900a4
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-05-03 13:12:08 -0300

    avancando

--- main.sql
+++ main.sql
@@ -1,5 +1,5 @@
 /*
-Timestamp é data e hora
+  https://onecompiler.com/postgresql
 */
 
 create table aluno (
@@ -15,14 +15,28 @@ create table aluno (
 
 INSERT INTO aluno (nome,cpf,idade,data_nascimento,ativo) VALUES ('KATHELYN','123.456.789-10',24,'1998-07-05',TRUE);
 INSERT INTO aluno (nome,cpf,idade,data_nascimento,ativo) VALUES ('JOANA','147.258.369-10',26,'1996-07-02',TRUE);
+INSERT INTO aluno (nome,cpf,idade,data_nascimento,ativo) VALUES ('KEMILLY','159.357.852-10',20,'2002-05-22',FALSE);
 SELECT * FROM aluno;
 
 UPDATE aluno SET nome = 'KELLY'  WHERE id=2;
 
 
-DELETE FROM aluno where id=1;
+DELETE FROM aluno WHERE id=2;
 SELECT * FROM aluno;
 
 /*
-  https://onecompiler.com/postgresql
+não usar o parêntese com idade AS idade_atual
 */
+SELECT nome,cpf,idade AS idade_atual FROM aluno;
+SELECT nome AS "Nome do Aluno",cpf,idade FROM aluno; /*usar as aspas duplas nesse caso*/
+
+/* <> != diferente  */
+
+SELECT * from aluno WHERE nome <> 'KEMILLY';
+
+/*
+LIKE
+*/
+SELECT * FROM aluno WHERE nome LIKE '_EMILLY';
+SELECT * FROM aluno WHERE nome NOT LIKE '_EMILLY';
+SELECT * FROM aluno WHERE nome LIKE 'K%';
