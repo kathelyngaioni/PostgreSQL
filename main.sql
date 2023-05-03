4626db4d7175aaab3d0d31f5e47e1d68f2ae09ff
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-05-03 08:58:24 -0300

    avancando

--- main.sql
+++ main.sql
@@ -5,7 +5,7 @@ Timestamp é data e hora
 create table aluno (
   id SERIAL primary key,
   nome VARCHAR(255),
-  cpf CHAR(11),
+  cpf TEXT,
   observacao TEXT,
   idade INTEGER,
   data_nascimento DATE,
@@ -14,4 +14,15 @@ create table aluno (
   );
 
 INSERT INTO aluno (nome,cpf,idade,data_nascimento,ativo) VALUES ('KATHELYN','123.456.789-10',24,'1998-07-05',TRUE);
+INSERT INTO aluno (nome,cpf,idade,data_nascimento,ativo) VALUES ('JOANA','147.258.369-10',26,'1996-07-02',TRUE);
 SELECT * FROM aluno;
+
+UPDATE aluno SET nome = 'KELLY'  WHERE id=2;
+
+
+DELETE FROM aluno where id=1;
+SELECT * FROM aluno;
+
+/*
+  https://onecompiler.com/postgresql
+*/
